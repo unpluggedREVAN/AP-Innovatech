041bd3b089f167b6b216aa23548100f1f9636f79
REVAN <joseaguero11@estudiantec.cr>
2024-03-25 12:05:42 -0600

    update: expo project changed

---
+++ android_proj/innovatech2/App.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aquí puedes añadir la lógica para manejar el inicio de sesión
+    console.log('Login con:', email, password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>InnovaTech</Text>
+      <Text style={styles.header}>Plataforma de Gestión de Proyectos</Text>
+
+      <Image
+        source={require('./assets/logo_nofondo.png')} //
+        style={styles.logo}
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Correo:"
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Contraseña:"
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.linkText}>¿Olvidé mi contraseña?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.linkText}>¿No tenés cuenta? Registrate aquí</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#4e9ec5',
+    alignSelf: 'center',
+    margin: 10,
+  },
+  header: {
+    fontSize: 18,
+    color: '#333',
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: '#ccc',
+  },
+  button: {
+    backgroundColor: '#4e9ec5',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    margin: 12,
+  },
+  logo: {
+    width: 200, // Ancho de la imagen
+    height: 200, // Altura de la imagen
+    resizeMode: 'contain', // Esto asegura que toda la imagen se ajuste dentro de las dimensiones dadas
+    alignSelf: 'center', // Esto centrará tu imagen en el contenedor de la vista
+    marginTop: 30, // Ajusta este valor para posicionar la imagen verticalmente
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  linkText: {
+    color: '#4e9ec5',
+    alignSelf: 'center',
+    marginTop: 15,
+  },
+});
