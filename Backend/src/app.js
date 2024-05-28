1c74cc1d868cbb9a73f4be26694a92f484e93f94
Drio02 <90213776+Drio02@users.noreply.github.com>
2024-05-28 01:11:17 -0600

    UPDATE - Database Connect

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,11 +4,16 @@ import { projectRoutes } from './routes/proyectos.routes.js'
 import { meetingRoutes } from './routes/reuniones.routes.js'
 import { taskRoutes } from './routes/tareas.routes.js'
 import { userRoutes } from './routes/users.routes.js'
+import mongoose from 'mongoose'
 
 const app = express();
 
 //Configuracion de la app
 app.use(express.json())
+
+//Conexion a MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Conectado a MongoDB Atlas'))
+.catch(err => console.error('Error al conectar a MongoDB', err));
 
 //Rutas de la app
 app.use('/Innovatech', authRoutes)
