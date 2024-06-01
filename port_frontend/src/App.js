ce94d164a76b441a0e27ebf1a162ad0ddf0fb118
REVAN <joseaguero11@estudiantec.cr>
2024-06-01 17:47:39 -0600

    update: reuniones and cuenta screens added (working)

--- port_frontend/src/App.js
+++ port_frontend/src/App.js
@@ -4,6 +4,8 @@ import LoginScreen from './Login';
 import RegisterScreen from './Register';
 import HomeScreen from './Home';
 import ColaboradoresScreen from './Colaboradores';
+import ReunionesScreen from './Reuniones';
+import CuentaScreen from './Cuenta';
 import './App.css';
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
           <Route path="/register" element={<RegisterScreen />} />
           <Route path="/main" element={<HomeScreen />} />
           <Route path="/colaboradores" element={<ColaboradoresScreen />} />
-          {/* Aquí puedes agregar más rutas para nuevas pantallas en el futuro */}
+          <Route path="/reuniones" element={<ReunionesScreen />} />
+          <Route path="/cuenta" element={<CuentaScreen />} />
+          {/* aquí se van agregando más */}
         </Routes>
       </div>
     </Router>
