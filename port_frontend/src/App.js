073b2341526cdb8e8b681e7c3e1979bf3e882960
REVAN <joseaguero11@estudiantec.cr>
2024-06-02 00:35:47 -0600

    update: modTareas and gestColab added (not styled)

--- port_frontend/src/App.js
+++ port_frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginScreen from './Login';
-import RegisterScreen from './Register';
 import HomeScreen from './Home';
 import ColaboradoresScreen from './Colaboradores';
 import ReunionesScreen from './Reuniones';
@@ -12,28 +10,27 @@ import CrearReunionScreen from './CrearReunion';
 import ProjectDetailsScreen from './ProjectDetails';
 import ColaboradorDetailsScreen from './ColaboradorDetails';
 import ReunionDetailsScreen from './ReunionDetails';
-import './App.css';
+import ModificarTareasScreen from './ModificarTareas';
+import GestionarColaboradoresScreen from './GestionarColaboradores'; // Importar la nueva pantalla
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/main" element={<HomeScreen />} />
-          <Route path="/colaboradores" element={<ColaboradoresScreen />} />
-          <Route path="/colaborador-detalles/:colaboradorId" element={<ColaboradorDetailsScreen />} />
-          <Route path="/reuniones" element={<ReunionesScreen />} />
-          <Route path="/reunion-detalles/:reunionId" element={<ReunionDetailsScreen />} />
-          <Route path="/evaluacion" element={<EvaluacionScreen />} />
-          <Route path="/cuenta" element={<CuentaScreen />} />
-          <Route path="/crear-proyecto" element={<CrearProyectoScreen />} />
-          <Route path="/crear-reunion" element={<CrearReunionScreen />} />
-          <Route path="/proyecto-detalles/:proyectoId" element={<ProjectDetailsScreen />} />
-          {/* Aquí puedes agregar más rutas para nuevas pantallas en el futuro */}
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/main" element={<HomeScreen />} />
+        <Route path="/colaboradores" element={<ColaboradoresScreen />} />
+        <Route path="/reuniones" element={<ReunionesScreen />} />
+        <Route path="/evaluacion" element={<EvaluacionScreen />} />
+        <Route path="/cuenta" element={<CuentaScreen />} />
+        <Route path="/crear-proyecto" element={<CrearProyectoScreen />} />
+        <Route path="/crear-reunion" element={<CrearReunionScreen />} />
+        <Route path="/proyecto-detalles/:proyectoId" element={<ProjectDetailsScreen />} />
+        <Route path="/colaborador-detalles/:colaboradorId" element={<ColaboradorDetailsScreen />} />
+        <Route path="/reunion-detalles/:reunionId" element={<ReunionDetailsScreen />} />
+        <Route path="/modificar-tareas/:proyectoId" element={<ModificarTareasScreen />} />
+        <Route path="/gestionar-colaboradores/:proyectoId" element={<GestionarColaboradoresScreen />} /> {/* Nueva ruta */}
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
     </Router>
   );
 }
